691c24d7a9ea2d667744e3c5793fb076dfaa8d7b
Igor Rodrigues Machado <107290125+igorrm19@users.noreply.github.com>
2026-02-27 00:22:18 -0300

    Remove unnecessary whitespace in product model

--- backEnd/model/products.model.js
+++ backEnd/model/products.model.js
@@ -2,7 +2,6 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-
     name: {
         type: String,
         required: true,
@@ -10,45 +9,35 @@ const productSchema = new mongoose.Schema({
         trim: true,
         lowercase: true
     },
-
     description: {
         type: String,
         trim: true,
         lowercase: true
     },
-
     price: {
         type: Number,
         required: true,
         min: 0
     },
-
     category: {
         type: String,
         trim: true,
         lowercase: true
     },
-
     subCategory: {
         type: String,
         trim: true,
         lowercase: true
     },
-
     tags: [String],
-
     stock: {
         type: Number,
         min: 0
     },
-
     brand: {
         type: String,
         default: "Skin Renew"
     },
-
     image: String
-
 }, { timestamps: true });
-
 module.exports = mongoose.model("Product", productSchema);
